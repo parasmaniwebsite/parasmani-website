e07085717d9c0917550335e2d8c6f2dd26872907
parasmaniwebsite <parasmaniwebsite@gmail.com>
2026-09-23 12:09:13 +0530

    Update app.js
    added sitemap route

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ import testRoute from "./routes/testRoute.js";
 import contactRoute from "./routes/contactRoute.js";
 import blogRoute from "./routes/blogRoute.js";
 import categoryRoutes from "./routes/categoryRoute.js";
+import sitemapRoute from "./routes/sitemapRoute.js";
 
 const app = express();
 
@@ -60,6 +61,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/contact", contactRoute);
 
 app.use("/api/blog", blogRoute);
+app.use("/api/sitemap.xml", sitemapRoute);
 
 app.use("/api/category", categoryRoutes);
 
