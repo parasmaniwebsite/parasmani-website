88c9c7c1b6bdf21f4c34bb9144672ed5b1760794
parasmaniwebsite <parasmaniwebsite@gmail.com>
2026-09-23 12:45:20 +0530

    Delete backend/src/routes/sitemapRoute.js

--- backend/src/routes/sitemapRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getSitemap } from "../controllers/sitemapController.js";
-
-const router = express.Router();
-
-router.get("/", getSitemap);
-
-export default router;
